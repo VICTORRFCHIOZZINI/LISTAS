2139114f812224ee2fa6745750459603a2af80ce
VICTORRFCHIOZZINI <127430122+VICTORRFCHIOZZINI@users.noreply.github.com>
2023-06-02 11:39:23 -0300

    Update PRODUTO.js

--- PRODUTO.js
+++ PRODUTO.js
@@ -1,6 +1,4 @@
 [
   {"ID": 1, "NOME": "A", "MARCA": "A", "PREÇO": 1},
-  {"ID": 2, "NOME": "B", "MARCA": "B", "PREÇO": 2},
-  {"ID": 3, "NOME": "C", "MARCA": "C", "PREÇO": 3},
-  {"ID": 4, "NOME": "D", "MARCA": "D", "PREÇO": 4}
+  {"ID": 2, "NOME": "B", "MARCA": "B", "PREÇO": 2}
 ]
