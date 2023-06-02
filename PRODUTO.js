2b54dd5a40357b26e7824eb35236e23baf1bbf4b
VICTORRFCHIOZZINI <127430122+VICTORRFCHIOZZINI@users.noreply.github.com>
2023-06-02 11:31:20 -0300

    Create PRODUTO.js

---
+++ PRODUTO.js
@@ -0,0 +1,6 @@
+[
+  {"ID": 1, "NOME": "A", "MARCA": "A", "PREÇO": 1},
+  {"ID": 2, "NOME": "B", "MARCA": "B", "PREÇO": 2},
+  {"ID": 3, "NOME": "C", "MARCA": "C", "PREÇO": 3},
+  {"ID": 4, "NOME": "D", "MARCA": "D", "PREÇO": 4}
+]
